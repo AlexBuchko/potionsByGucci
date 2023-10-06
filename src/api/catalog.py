6dea1ee6c690b1b1f26bcdc638ebfb75f8c2840c
AlexBuchko <abuchko@calpoly.edu>
2023-10-06 09:43:39 -0700

    uh mb

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -12,10 +12,6 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
     inventory = db.get_global_inventory()
-    if (inventory["num_red_potions"]) <= 0:
-        print("no potions to sell, returning")
-        return
-    ans = []
     colors = ["red", "green", "blue"]
     for color in colors:
         key = f"num_{color}_potions"
