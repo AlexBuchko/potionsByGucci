38defefa5616b64741c0f0ce31ef6198dbed355b
AlexBuchko <abuchko@calpoly.edu>
2023-10-09 18:48:05 -0700

    updating barrels logic to try to get 1 of each barrel

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 from src.api import database as db
+from src.api import colors
 
 router = APIRouter(
     prefix="/barrels",
@@ -30,14 +31,9 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
         json_string = json.dumps(barrel.__dict__)
         print(json_string)
 
-    colors = ["red", "green", "blue"]
     for barrel in barrels_delivered:
         # getting color
-        color = None
-        for i in range(len(colors)):
-            if barrel.potion_type[i] == 1:
-                color = colors[i]
-                break
+        color = colors.get_color_from_potion_type(barrel.potion_type)
 
         inventory = db.get_global_inventory()
         money_spent = barrel.price * barrel.quantity
@@ -50,29 +46,8 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
     return "OK"
 
 
-# Gets called once a day
-@router.post("/plan")
-def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
-    """ """
-
-    print(wholesale_catalog)
-    # printing the dict version of this for testing later down the road
-    for barrel in wholesale_catalog:
-        json_string = json.dumps(barrel.__dict__)
-        print(json_string)
-
-    BARRELS_TO_BUY = {
-        "SMALL_RED_BARREL",
-        "SMALL_BLUE_BARREL",
-        "SMALL_GREEN_BARREL",
-    }
-
+def balance_barrels(catalog):
     # goal is to keep a balance of potion types
-    catalog = {
-        barrel.sku: barrel
-        for barrel in wholesale_catalog
-        if barrel.sku in BARRELS_TO_BUY
-    }
 
     inventory = db.get_global_inventory()
     # getting a count of how many potions we have
@@ -96,24 +71,67 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         if i == 0:  # recomputing the order when we reset
             buying_order = sorted(colors, key=lambda color: potion_counts[color])
         color_to_buy = buying_order[i]
-        barrel = catalog[f"SMALL_{color_to_buy.upper()}_BARREL"]
+        sku = f"SMALL_{color_to_buy.upper()}_BARREL"
+        barrel = catalog[sku]
         can_afford = gold >= barrel.price
         if can_afford:
             # adding to purchase plan
             potion_counts[color_to_buy] += barrel.ml_per_barrel / 100
-            purchase_plan[color_to_buy] = purchase_plan.get(color_to_buy, 0) + 1
+            purchase_plan[sku] = purchase_plan.get(color_to_buy, 0) + 1
             gold -= barrel.price
             i = 0
         else:
             i += 1
 
     # getting a return value
+    return purchase_plan
+
+
+sizes = ["MINI", "SMALL", "MEDIUM", "LARGE"]
+sizes_to_buy = ["MINI", "SMALL"]
+
+
+def one_of_each(catalog):
+    # buying the smallest amount of a each color we don't have that's for sale
+    inventory = db.get_global_inventory()
+    gold = inventory["gold"]
+    potion_counts = db.get_net_potion_counts()
+    purchase_plan = {}
+    for color in colors.colors:
+        if potion_counts[color] > 0:
+            continue
+        for size in sizes_to_buy:
+            sku = f"{size}_{color.upper()}_BARREL"
+            if sku in catalog:
+                barrel = catalog[sku]
+                if barrel.price <= gold:
+                    potion_counts[color] = (
+                        potion_counts.get(color, 0) + barrel.ml_per_barrel
+                    )
+                    gold -= barrel.price
+                    purchase_plan[sku] = 1
+                    break
+    return purchase_plan
+
+
+# Gets called once a day
+@router.post("/plan")
+def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
+    """ """
+
+    print(wholesale_catalog)
+    # printing the dict version of this for testing later down the road
+    for barrel in wholesale_catalog:
+        json_string = json.dumps(barrel.__dict__)
+        print(json_string)
+
+    catalog = {barrel.sku: barrel for barrel in wholesale_catalog}
     ans = []
+    purchase_plan = one_of_each(catalog)
     print(purchase_plan)
-    for color, count in purchase_plan.items():
-        sku = f"SMALL_{color.upper()}_BARREL"
+    for sku, count in purchase_plan.items():
         barrel = catalog[sku]
-        print(f"purchasing {count} { color } barrels at {barrel.price}")
+        print(f"purchasing {sku} at {barrel.price}")
         ans.append(
             {
                 "sku": sku,
--- src/api/database.py
+++ src/api/database.py
@@ -1,6 +1,7 @@
 import os
 import dotenv
 from sqlalchemy import create_engine, text
+from src.api import colors
 
 
 def database_connection_url():
@@ -20,6 +21,19 @@ def get_global_inventory():
         current_inventory = connection.execute(select_statement)
         current_inventory = current_inventory.first()._asdict()
         return current_inventory
+
+
+def get_net_potion_counts():
+    inventory = get_global_inventory()
+    potion_counts = {
+        color: inventory[f"num_{color}_potions"] for color in colors.colors
+    }
+    fluid_counts = {color: inventory[f"num_{color}_ml"] for color in colors.colors}
+
+    # if we have have 350ml of red fluid, we might as well have 3.5 red potions
+    for color, num_ml in fluid_counts.items():
+        potion_counts[color] += num_ml / 100
+    return potion_counts
 
 
 def execute(command):
